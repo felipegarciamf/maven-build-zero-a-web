ece7cb70407667eab5b70e49b7ee99d7b2a2460f
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-09-10 23:14:23 -0300

    alteracao de valor

--- produtos/src/main/java/br/com/alura/maven/App.java
+++ produtos/src/main/java/br/com/alura/maven/App.java
@@ -8,7 +8,7 @@ public class App {
 	
 	public static void main(String[] args) {
 		Produto produto = new Produto("Teste", 0.14);
-		System.out.println("Hello World!");
+		System.out.println("Hello World!" + produto.getNome() + " e custa " + produto.getPreco());
 	}
 
 }
