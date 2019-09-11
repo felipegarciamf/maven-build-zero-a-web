93cf3a03f5e9a853cea9bec00539cac63163388b
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-09-10 22:27:28 -0300

    subindo gson

--- produtos/src/test/java/br/com/alura/maven/ProdutoTest.java
+++ produtos/src/test/java/br/com/alura/maven/ProdutoTest.java
@@ -1,9 +1,6 @@
 package br.com.alura.maven;
 
-import junit.framework.TestCase;
-import junit.framework.Test;
-import junit.framework.TestSuite;
 
-public class ProdutoTest extends TestCase {
+public class ProdutoTest {
 
 }
