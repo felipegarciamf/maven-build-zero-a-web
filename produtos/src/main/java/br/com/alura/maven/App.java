c3755e7336306b194d545a16e2b9c1c3849d3efd
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-09-10 22:51:51 -0300

    subindo versão com alteração

--- produtos/src/main/java/br/com/alura/maven/App.java
+++ produtos/src/main/java/br/com/alura/maven/App.java
@@ -4,10 +4,11 @@ package br.com.alura.maven;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+		Produto produto = new Produto("Teste", 0.14);
+		System.out.println("Hello World!");
+	}
+
 }
