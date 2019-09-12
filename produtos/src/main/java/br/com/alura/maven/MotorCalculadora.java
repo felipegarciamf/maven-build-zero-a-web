bc7f154fd80b337533a3906203c9c925ee92d6dc
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-09-11 23:16:07 -0300

    criando classe implement de calculadora para utilização de interface e designe pattern strategy

---
+++ produtos/src/main/java/br/com/alura/maven/MotorCalculadora.java
@@ -0,0 +1,11 @@
+package br.com.alura.maven;
+
+public class MotorCalculadora {
+	
+	
+	
+	public double calculaValor(Calculadora calc, double valor) throws Exception {
+		return calc.calculo(valor);
+	}
+
+}
--- produtos/src/test/java/br/com/alura/maven/CalculadoraTest.java
+++ produtos/src/test/java/br/com/alura/maven/CalculadoraTest.java
@@ -1,6 +1,5 @@
 package br.com.alura.maven;
 
-import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -9,6 +8,7 @@ public class CalculadoraTest extends TestCase {
 
 	static Calculadora cubico = new CalculoCubico();
 	static Calculadora raiz = new CalculoRaiz();
+	MotorCalculadora calcula = new MotorCalculadora();
 	
 	/**
 	 * Create the test case
@@ -32,28 +32,28 @@ public class CalculadoraTest extends TestCase {
      * @throws Exception 
      */
     public void testRaiz() throws Exception {
-    	assertEquals(1.0, raiz.calculo(1));
+    	assertEquals(1.0, calcula.calculaValor(raiz, 1));
     }
     
     public void testErroRaiz() throws Exception {
-    	assertNotSame(124, raiz.calculo(1));
+    	assertNotSame(124, calcula.calculaValor(raiz,1));
     }
     
     public void testErroNegativoRaiz() throws Exception {
-    	assertNotSame(0, raiz.calculo(-1));
+    	assertNotSame(0, calcula.calculaValor(raiz, -1));
     }
     
     
     public void testCubico() throws Exception {
-    	assertEquals(2.154434690031884, cubico.calculo(10));
+    	assertEquals(2.154434690031884, calcula.calculaValor(cubico,10));
     }
     
     public void testErroCubico() throws Exception {
-    	assertNotSame(12, cubico.calculo(10));
+    	assertNotSame(12, calcula.calculaValor(cubico,10));
     }
     
     public void testErroNegativoCubico() throws Exception {
-    	assertEquals(0.0, cubico.calculo(-10));
+    	assertEquals(0.0, calcula.calculaValor(cubico,-10));
     }
 
 }
